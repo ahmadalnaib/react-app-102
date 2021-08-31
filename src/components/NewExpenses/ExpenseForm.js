e8f2c9a50539e6b27e3b60eeaec26d7bf7495a5d
ahmedhabeeb73 <alnaib888@gmail.com>
2021-08-31 20:56:45 +0200

    add states

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-  const handleTitle = (e) => {
+  const [title, setTitle] = useState('');
+  const [amount, setAmount] = useState('');
+  const [date, setDate] = useState('');
 
+  const handleTitle = (e) => {
+    setTitle(e.target.value);
   };
+
+  const handleAmount = (e) => {
+    setAmount('e.target.value');
+  };
+
+  const handleDate = (e) => {
+    setDate(e.target.value);
+  };
+
   return (
     <form>
       <div className='new-expense__controls'>
@@ -15,12 +28,17 @@ const ExpenseForm = () => {
 
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input type='number' min='0.01' step='0.01' />
+          <input type='number' min='0.01' step='0.01' onChange={handleAmount} />
         </div>
 
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type='date' min='2020-01-01' max='2022-12-31' />
+          <input
+            type='date'
+            min='2020-01-01'
+            max='2022-12-31'
+            onChange={handleDate}
+          />
         </div>
       </div>
 
